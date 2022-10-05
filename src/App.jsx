6fcfdfbfe85bfefa62d30334774cf37a5d66a2d2
Luxe17 <hanson1717@live.com>
2022-10-05 16:48:43 -0600

    Added fetch to get game data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import './App.css'
-import Navbar from './components/Navbar'
+import { useEffect, useState } from "react"
+import StatTable from './components/StatTable'
 import Shotmap from './components/Shotmap'
+import Sidebar from './components/Sidebar'
+import '@tremor/react/dist/esm/tremor.css';
 
 function App() {
+  const [gameData, setGameData] = useState()
+  const [gameID, setGameID] = useState("2021030125")
+
+  useEffect(() => {
+    d3.selectAll(".shot").remove()
+
+    fetch(`https://statsapi.web.nhl.com/api/v1/game/${gameID}/feed/live`)
+    .then(res => res.json())
+    .then(data => setGameData(data))
+    
+  }, [gameID])
 
   return (
     <div className="App">
-      <Navbar />
-      <Shotmap />
+      <Sidebar onSelect={setGameID}/>
+      <div className="data-grid">
+        <Shotmap shotData={gameData} gameID={gameID} setGameData={setGameData}/>
+        <StatTable id={gameID} />
+      </div>
     </div>
   )
 }
