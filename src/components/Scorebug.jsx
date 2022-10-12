59510a5390758a3d88197dd0dfcef3de37682246
Luxe17 <hanson1717@live.com>
2022-10-12 11:25:26 -0600

    Added shots desc to card

--- src/components/Scorebug.jsx
+++ src/components/Scorebug.jsx
@@ -1,19 +1,17 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import Shotmap from "./Shotmap";
-
 export default function Scorebug({home, away}){
 
-    return <div className="stats stats-vertical lg:stats-horizontal shadow bg-primary text-primary-content self-start">
+    return <div className="stats shadow bg-primary text-primary-content">
   
     <div className="stat">
       <div className="stat-title tex">{home.name}</div>
       <div className="stat-value">{home.goals}</div>
+      <div className="stat-desc">{home.shots} shots</div>
     </div>
     
     <div className="stat">
       <div className="stat-title">{away.name}</div>
       <div className="stat-value">{away.goals}</div>
+      <div className="stat-desc">{away.shots} shots</div>
     </div>
     
   </div>
