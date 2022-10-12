8620bc7721a31a97df5445dd867ce45d36d8af55
Luxe17 <hanson1717@live.com>
2022-10-12 12:32:12 -0600

    Switched home and away positions

--- src/components/Scorebug.jsx
+++ src/components/Scorebug.jsx
@@ -1,17 +1,17 @@
 export default function Scorebug({home, away}){
 
     return <div className="stats shadow bg-primary text-primary-content">
-  
-    <div className="stat">
-      <div className="stat-title tex">{home.name}</div>
-      <div className="stat-value">{home.goals}</div>
-      <div className="stat-desc">{home.shots} shots</div>
-    </div>
     
     <div className="stat">
       <div className="stat-title">{away.name}</div>
       <div className="stat-value">{away.goals}</div>
       <div className="stat-desc">{away.shots} shots</div>
+    </div>
+
+    <div className="stat">
+      <div className="stat-title tex">{home.name}</div>
+      <div className="stat-value">{home.goals}</div>
+      <div className="stat-desc">{home.shots} shots</div>
     </div>
     
   </div>
